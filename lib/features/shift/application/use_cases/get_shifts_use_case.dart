8c6fba15482fd95babe9c37c7fb6a4cf140ca9e2
Fábio Moreira <106029405+fabio550@users.noreply.github.com>
2026-08-20 15:52:41 -0300

    Add import for shift_status enum in get_shifts_use_case

--- lib/features/shift/application/use_cases/get_shifts_use_case.dart
+++ lib/features/shift/application/use_cases/get_shifts_use_case.dart
@@ -1,5 +1,6 @@
 import 'package:driver_analytics_app/features/shift/domain/entities/shift_entity.dart';
 import 'package:driver_analytics_app/features/shift/domain/repositories/shift_repository.dart';
+import 'package:driver_analytics_app/features/shift/domain/enums/shift_status.dart';
 
 class GetShiftsUseCase {
   final ShiftRepository _repository;
