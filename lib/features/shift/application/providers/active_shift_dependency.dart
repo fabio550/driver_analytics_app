779b4f495af3bcd3ce148a9ed1367100c7d037dc
Fábio Moreira <106029405+fabio550@users.noreply.github.com>
2026-08-20 14:58:31 -0300

    Add active_shift_dependency.dart for shift use cases
    This file provides dependency injection for use cases related to shift management, including starting, pausing, and resuming shifts.

---
+++ lib/features/shift/application/providers/active_shift_dependency.dart
@@ -0,0 +1,37 @@
+import 'package:driver_analytics_app/core/infrastructure/services/uuid_generator_provider.dart';
+import 'package:driver_analytics_app/features/shift/application/providers/shift_dependency.dart';
+import 'package:driver_analytics_app/features/shift/application/use_cases/pause_shift_use_case.dart';
+import 'package:driver_analytics_app/features/shift/application/use_cases/resume_shift_use_case.dart';
+import 'package:driver_analytics_app/features/shift/application/use_cases/start_shift_use_case.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final startShiftUseCaseProvider = Provider<StartShiftUseCase>((ref) {
+  final repository = ref.watch(shiftRepositoryProvider);
+  final validator = ref.watch(shiftValidatorProvider);
+  final idGenerator = ref.watch(uuidGeneratorProvider);
+  return StartShiftUseCase(
+    repository: repository,
+    validator: validator,
+    idGenerator: idGenerator,
+  );
+});
+
+final pauseShiftUseCaseProvider = Provider<PauseShiftUseCase>((ref) {
+  final repository = ref.watch(shiftRepositoryProvider);
+  final validator = ref.watch(shiftValidatorProvider);
+  final idGenerator = ref.watch(uuidGeneratorProvider);
+  return PauseShiftUseCase(
+    repository: repository,
+    validator: validator,
+    idGenerator: idGenerator,
+  );
+});
+
+final resumeShiftUseCaseProvider = Provider<ResumeShiftUseCase>((ref) {
+  final repository = ref.watch(shiftRepositoryProvider);
+  final validator = ref.watch(shiftValidatorProvider);
+  return ResumeShiftUseCase(
+    repository: repository,
+    validator: validator,
+  );
+});
