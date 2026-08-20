9ac5ccd5a366a4507c89d1acdd8d78c53d626029
Fábio Moreira <106029405+fabio550@users.noreply.github.com>
2026-08-20 11:26:38 -0300

    Add optional index field to ValidationFailure class

--- lib/core/domain/failures/validation_failure.dart
+++ lib/core/domain/failures/validation_failure.dart
@@ -1,9 +1,11 @@
 class ValidationFailure<TField> {
   final TField field;
   final String message;
-
+  final int? index;
+  
   const ValidationFailure({
     required this.field,
     required this.message,
+    this.index,
   });
 }
