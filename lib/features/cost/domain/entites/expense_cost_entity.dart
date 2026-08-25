49e9b9ff83b12c45225ca0b923e41c3b87b58f26
Fábio Moreira <106029405+fabio550@users.noreply.github.com>
2026-08-25 16:18:51 -0300

    Add ExpenseCostEntity class for expense categorization

---
+++ lib/features/cost/domain/entites/expense_cost_entity.dart
@@ -0,0 +1,14 @@
+class ExpenseCostEntity extends CostEntity {
+  final ExpenseSubcategory subcategory;
+
+  const ExpenseCostEntity({
+    required super.id,
+    required super.amount,
+    required super.date,
+    super.description,
+    required this.subcategory,
+  });
+
+  @override
+  CostCategory get category => CostCategory.expense;
+}
