00ae1c451a5c98baff47928cca82f27399dfff8f
Fábio Moreira <106029405+fabio550@users.noreply.github.com>
2026-08-26 10:25:40 -0300

    Rename formatted to formattedHHmmss and update logic

--- lib/core/extensions/duration_extensions.dart
+++ lib/core/extensions/duration_extensions.dart
@@ -1,10 +1,10 @@
 extension DurationExtensions on Duration {
-  String get formatted {
-    String two(int value) => value.toString().padLeft(2, '0');
+  String get formattedHHmmss {
+    String two(int n) => n.toString().padLeft(2, '0');
 
     final hours = two(inHours);
-    final minutes = two(inMinutes.remainder(60));
-    final seconds = two(inSeconds.remainder(60));
+    final minutes = two(inMinutes % 60);
+    final seconds = two(inSeconds % 60);
 
     return '$hours:$minutes:$seconds';
   }
