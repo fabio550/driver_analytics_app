4dbf6c19806229dfb5e7271ef0e3b55fa19184a8
Fábio Moreira <106029405+fabio550@users.noreply.github.com>
2026-08-27 16:34:22 -0300

    Add standardized border radius constants

---
+++ lib/core/presentation/theme/app_radius.dart
@@ -0,0 +1,8 @@
+/// Raios de borda padronizados do app.
+class AppRadius {
+  const AppRadius._();
+
+  static const double sm = 8;
+  static const double md = 12;
+  static const double lg = 16;
+}
