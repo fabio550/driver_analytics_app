a5444d00b673f14249958a05a3ca3ec3dfebc9ea
Fábio Moreira <106029405+fabio550@users.noreply.github.com>
2026-08-27 16:35:46 -0300

    Add light and dark themes to MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:driver_analytics_app/core/presentation/theme/app_theme.dart';
 import 'package:driver_analytics_app/core/routes/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -14,6 +15,9 @@ void main() async {
           return MaterialApp.router(
             title: 'Driver Analytics',
             debugShowCheckedModeBanner: false,
+            theme: AppTheme.light,
+            darkTheme: AppTheme.dark,
+            themeMode: ThemeMode.system,
             routerConfig: router,
           );
         },
